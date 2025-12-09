4e54c6753b6dad23d4490864efef36943244a74b
cidarosa <lucita.rosa@gmail.com>
2025-12-09 15:38:48 -0300

    feat: integração com RestClient e Status Code para exception na integração

--- src/main/java/com/github/cidarosa/acrsensor/temperature/monitoring/api/controller/SensorMonitoringController.java
+++ src/main/java/com/github/cidarosa/acrsensor/temperature/monitoring/api/controller/SensorMonitoringController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/monitoring")
@@ -40,6 +41,9 @@ public class SensorMonitoringController {
     public void enable(@PathVariable TSID sensorId) {
 
         SensorMonitoring sensorMonitoring = findByIdOrDefault(sensorId);
+        if(sensorMonitoring.getEnbled()){
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
         sensorMonitoring.setEnbled(true);
         sensorMonitoringRepository.saveAndFlush(sensorMonitoring);
     }
